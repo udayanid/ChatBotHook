61e544b51d23058e32efb822d21c2a2f24232800
Udayakumar <udhayan.addon@gmail.com>
2018-08-09 00:41:50 +0530

    Added Message rest api for hook

--- src/main/java/it/sella/controllers/ChatBotHookController.java
+++ src/main/java/it/sella/controllers/ChatBotHookController.java
@@ -2,6 +2,7 @@ package it.sella.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,5 +16,10 @@ public class ChatBotHookController {
 	@PostMapping("/query")
 	public String getQuery(@RequestParam("message") String message) {
 		return "Got the message " + message;
+	}
+
+	@PostMapping("/message")
+	public String getMessage(@RequestBody String message) {
+		return message;
 	}
 }
