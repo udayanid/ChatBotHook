f301b8417f8227df8fab5ac077a90102cffc07d2
Udayakumar <udhayan.addon@gmail.com>
2018-08-09 01:18:44 +0530

    added verify api

--- src/main/java/it/sella/controllers/ChatBotHookController.java
+++ src/main/java/it/sella/controllers/ChatBotHookController.java
@@ -2,7 +2,6 @@ package it.sella.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.ResponseEntity.BodyBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,6 +18,12 @@ public class ChatBotHookController {
 	@PostMapping("/query")
 	public String getQuery(@RequestParam("message") String message) {
 		return "Got the message " + message;
+	}
+
+	@PostMapping("verify")
+	public ResponseEntity<?> verify(@RequestParam("hub_challenge") String challenge,
+			@RequestParam("hub_verify_token") String token) {
+		return new ResponseEntity<String>(challenge, HttpStatus.OK);
 	}
 
 	@PostMapping("/message")
