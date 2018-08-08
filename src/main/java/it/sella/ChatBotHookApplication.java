a32fe4ee1e5ee9de3e58ece256b0edbe5a9995ca
Udayakumar <udhayan.addon@gmail.com>
2018-08-08 23:53:32 +0530

    simple Rest Api for webhook

---
+++ src/main/java/it/sella/ChatBotHookApplication.java
@@ -0,0 +1,12 @@
+package it.sella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBotHookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBotHookApplication.class, args);
+	}
+}
---
+++ src/main/java/it/sella/controllers/ChatBotHookController.java
@@ -0,0 +1,19 @@
+package it.sella.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatBotHookController {
+	@GetMapping("/")
+	public String sayHello() {
+		return "Ready to hook";
+	}
+
+	@PostMapping("/query")
+	public String getQuery(@RequestParam("message") String message) {
+		return "Got the message " + message;
+	}
+}
