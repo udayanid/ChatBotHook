e6aa2b88e1d93a0eca3d6b559dd98a406b3965e5
Udayakumar <udhayan.addon@gmail.com>
2018-08-11 00:04:56 +0530

    Implemented messenger4j

--- src/main/java/it/sella/ChatBotHookApplication.java
+++ src/main/java/it/sella/ChatBotHookApplication.java
@@ -1,10 +1,20 @@
 package it.sella;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.github.messenger4j.Messenger;
 
 @SpringBootApplication
 public class ChatBotHookApplication {
+	 @Bean
+	    public Messenger messenger(@Value("${messenger4j.pageAccessToken}") String pageAccessToken,
+	                               @Value("${messenger4j.appSecret}") final String appSecret,
+	                               @Value("${messenger4j.verifyToken}") final String verifyToken) {
+	        return Messenger.create(pageAccessToken, appSecret, verifyToken);
+	    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatBotHookApplication.class, args);
--- src/main/java/it/sella/controllers/ChatBotHookController.java
+++ src/main/java/it/sella/controllers/ChatBotHookController.java
@@ -1,9 +1,12 @@
 package it.sella.controllers;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import static com.github.messenger4j.Messenger.SIGNATURE_HEADER_NAME;
+import static java.util.Optional.of;
 
-import org.springframework.http.HttpRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,11 +14,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 import org.springframework.web.bind.annotation.RestController;
+
+import com.github.messenger4j.Messenger;
+import com.github.messenger4j.exception.MessengerApiException;
+import com.github.messenger4j.exception.MessengerIOException;
+import com.github.messenger4j.exception.MessengerVerificationException;
+import com.github.messenger4j.send.MessagePayload;
+import com.github.messenger4j.send.MessagingType;
+import com.github.messenger4j.send.message.TextMessage;
 
 @RestController
 public class ChatBotHookController {
+	private static final Logger logger = LoggerFactory.getLogger(ChatBotHookController.class);
+	final Messenger messenger;
+
+	@Autowired
+	public ChatBotHookController(Messenger messenger) {
+		this.messenger = messenger;
+	}
 
 	@GetMapping("/webhook")
 	public ResponseEntity<?> verify(@RequestParam("hub.challenge") String challenge,
@@ -35,8 +53,31 @@ public class ChatBotHookController {
 
 	@PostMapping("/webhook")
 	public ResponseEntity<?> getMessage(@RequestBody final String payLoad,
-			@RequestHeader("X-Hub-Signature") final String signature) {
-		System.out.println(signature);
+			@RequestHeader(SIGNATURE_HEADER_NAME) final String signature) {
+		logger.debug("Received Messenger Platform callback - payload: {} | signature: {}", payLoad, signature);
+		try {
+			this.messenger.onReceiveEvents(payLoad, of(signature), event -> {
+				final String senderId = event.senderId();
+				if (event.isTextMessageEvent()) {
+					final String text = event.asTextMessageEvent().text();
+
+					final TextMessage textMessage = TextMessage.create(text);
+					final MessagePayload messagePayload = MessagePayload.create(senderId, MessagingType.RESPONSE,
+							textMessage);
+
+					try {
+						messenger.send(messagePayload);
+					} catch (MessengerApiException | MessengerIOException e) {
+						logger.warn("Processing of callback payload failed: {}", e.getMessage());
+					}
+				}
+
+			});
+		} catch (MessengerVerificationException e) {
+			logger.warn("Processing of callback payload failed: {}", e.getMessage());
+			return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+		}
+		logger.debug("Processed callback payload successfully");
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 
