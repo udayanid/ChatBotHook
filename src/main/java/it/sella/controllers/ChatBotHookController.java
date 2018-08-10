9ed99ba4de9987d3b09253ac436a7f97389645d2
Udayakumar <udhayan.addon@gmail.com>
2018-08-10 22:30:01 +0530

    Fixed the message issue

--- src/main/java/it/sella/controllers/ChatBotHookController.java
+++ src/main/java/it/sella/controllers/ChatBotHookController.java
@@ -10,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ChatBotHookController {
-	
-	@GetMapping("/")
+
+	@GetMapping("/webhook")
 	public ResponseEntity<?> verify(@RequestParam("hub.challenge") String challenge,
 			@RequestParam("hub.verify_token") String token) {
 		if (token.equals("mycustomtoken23"))
@@ -20,9 +20,15 @@ public class ChatBotHookController {
 			return new ResponseEntity<>(HttpStatus.FORBIDDEN);
 	}
 
-	@PostMapping("/")
+	@PostMapping("/webhook")
 	public ResponseEntity<?> getMessage(@RequestBody String message) {
 		System.out.println(message);
 		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	@GetMapping("/")
+	public ResponseEntity<?> sayConnected() {
+
+		return new ResponseEntity<String>("Webhook is Listening", HttpStatus.OK);
 	}
 }
