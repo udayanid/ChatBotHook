0d1cea562d8cd8514a6c39561997387bb4940759
Udayakumar <udhayan.addon@gmail.com>
2018-08-13 00:15:53 +0530

    Connected Cognitive Service

--- src/main/java/it/sella/controllers/ChatBotHookController.java
+++ src/main/java/it/sella/controllers/ChatBotHookController.java
@@ -1,6 +1,9 @@
 package it.sella.controllers;
 
 import static com.github.messenger4j.Messenger.SIGNATURE_HEADER_NAME;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import static java.util.Optional.of;
 
 import org.slf4j.Logger;
@@ -8,6 +11,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +28,7 @@ import com.github.messenger4j.exception.MessengerVerificationException;
 import com.github.messenger4j.send.MessagePayload;
 import com.github.messenger4j.send.MessagingType;
 import com.github.messenger4j.send.message.TextMessage;
+import com.github.messenger4j.webhook.Event;
 
 @RestController
 public class ChatBotHookController {
@@ -44,32 +49,15 @@ public class ChatBotHookController {
 			return new ResponseEntity<>(HttpStatus.FORBIDDEN);
 	}
 
-	// @PostMapping("/webhook")
-	// public ResponseEntity<?> getMessage(HttpServletRequest
-	// request,HttpServletResponse response) {
-	// System.out.println(request.getContentType());
-	// return new ResponseEntity<>(HttpStatus.OK);
-	// }
-
 	@PostMapping("/webhook")
 	public ResponseEntity<?> getMessage(@RequestBody final String payLoad,
 			@RequestHeader(SIGNATURE_HEADER_NAME) final String signature) {
 		logger.debug("Received Messenger Platform callback - payload: {} | signature: {}", payLoad, signature);
 		try {
 			this.messenger.onReceiveEvents(payLoad, of(signature), event -> {
-				final String senderId = event.senderId();
+
 				if (event.isTextMessageEvent()) {
-					final String text = event.asTextMessageEvent().text();
-
-					final TextMessage textMessage = TextMessage.create(text);
-					final MessagePayload messagePayload = MessagePayload.create(senderId, MessagingType.RESPONSE,
-							textMessage);
-
-					try {
-						messenger.send(messagePayload);
-					} catch (MessengerApiException | MessengerIOException e) {
-						logger.warn("Processing of callback payload failed: {}", e.getMessage());
-					}
+					processTextMessage(event);
 				}
 
 			});
@@ -81,9 +69,32 @@ public class ChatBotHookController {
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 
+	private void processTextMessage(Event event) {
+		final String senderId = event.senderId();
+		final String text = event.asTextMessageEvent().text();
+		RestTemplate restTemplate = new RestTemplate();
+
+		final String url = "https://messengerqna.azurewebsites.net/qnamaker/knowledgebases/2130e964-ee28-4340-bef6-4af81d916292/generateAnswer";
+		String json = "{\"question\":\"what can be sell on amazon?\"}";
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Authorization", "EndpointKey 1d5815e4-34dd-46be-8d3e-e8619b7de192");
+		headers.setContentType(MediaType.APPLICATION_JSON);
+
+		HttpEntity<String> entity = new HttpEntity(json, headers);
+		String answer = restTemplate.postForObject(url, entity, String.class);
+		final TextMessage textMessage = TextMessage.create(answer);
+		final MessagePayload messagePayload = MessagePayload.create(senderId, MessagingType.RESPONSE, textMessage);
+
+		try {
+			messenger.send(messagePayload);
+		} catch (MessengerApiException | MessengerIOException e) {
+			logger.warn("Processing of callback payload failed: {}", e.getMessage());
+		}
+
+	}
+
 	@GetMapping("/")
 	public ResponseEntity<?> sayConnected() {
-
 		return new ResponseEntity<String>("Webhook is Listening", HttpStatus.OK);
 	}
 }
